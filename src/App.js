c2ece219a4829ddbdc9b152993f149cca1b86980
aggarabedian <abrahamgarabedian@gmail.com>
2022-07-22 13:22:47 -0700

    Adds interpolation to fetch url, buttons to increase/decrease : next/previous, adds Loader

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
 import React, { useEffect, useState} from 'react';
 import './App.css';
+import Loader from './Loader';
 
 function App() {
   const [error, setError] = useState(null);
   const [painting, setPainting] = useState({});
-  
+  const [url, setUrl] = useState(436121);
+  const [isLoaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
-    fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/436121')
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${url}`)
       .then(res => res.json())
       .then(
         (result) => {
+          setIsLoaded(true);
           setPainting(result);
         },
         (error) => {
+          setIsLoaded(true);
           setError(error);
         }
         )
-      }, [])
+      })
 
   if (error) {
     return (<div>Error: {error.message}</div>)
+  } else if (!isLoaded) {
+    return (
+      <Loader />
+    )
   } else {
     return (
       <div className='container'>
-        <img src={painting.primaryImage} alt={painting.title}/>
+        <div className='images'>
+          <img src={painting.primaryImageSmall} alt={painting.title}/>
+        </div>
         <h2>
           {painting.title}
         </h2>
         <h3>
           {painting.artistDisplayName}
         </h3>
+        <div>
+          <button onClick={() => setUrl(url - 1)}>Previous Painting</button>
+          <button onClick={() => setUrl(url + 1)}>Next Painting</button>
+        </div>
       </div>
   )
   }
