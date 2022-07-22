c6158e56fdee1544adb33f545e3f76ddca02a781
aggarabedian <abrahamgarabedian@gmail.com>
2022-07-21 17:43:49 -0700

    Adds fetch call for API, displays image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import React, { useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [error, setError] = useState(null);
+  const [painting, setPainting] = useState({});
+  
+  useEffect(() => {
+    fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/436121')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setPainting(result);
+        },
+        (error) => {
+          setError(error);
+        }
+        )
+      }, [])
+      console.log("### painting ###", painting);
+
   return (
-    <div></div>
+    <div className='container'>
+      <img src={painting.primaryImage} alt={painting.title}/>
+    </div>
   )
 }
 
